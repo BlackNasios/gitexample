9b267b9c8ddb58cc13b8ded728047f94b5c97a5b
BlackNasios <sotirismavronasios@gmail.com>
2019-05-16 20:22:45 +0300

    1st commit of this example

---
+++ index.js
@@ -0,0 +1,25 @@
+function my_function1(stars){
+    for (i=0; i<stars; i++){
+        var a="*";
+
+    console.log("*"+a.repeat(i));
+
+    }
+    
+    
+}
+
+my_function1(10)
+
+
+
+
+    
+
+
+
+
+
+
+     
+
